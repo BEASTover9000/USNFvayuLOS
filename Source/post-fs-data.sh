fa6bd9b3fd5212a1f7a90dd10a3d625779030b4b
ToucH <85633117+ToucH9000@users.noreply.github.com>
2023-09-06 01:40:53 +0500

    Update post-fs-data.sh

--- Source/post-fs-data.sh
+++ Source/post-fs-data.sh
@@ -2,15 +2,10 @@
 
 MODDIR="${0%/*}"
 MODNAME="${MODDIR##*/}"
-MAGISKTMP="$(magisk --path)" || MAGISKTMP=/sbin
 
 # Remove Play Services from the Magisk Denylist when set to enforcing.
 if magisk --denylist status; then
 	magisk --denylist rm com.google.android.gms
-fi
-
-if [ ! -e "$MAGISKTMP/.magisk/mirror/sepolicy.rules/$MODNAME/sepolicy.rule" ] && [ ! -e "$MAGISKTMP/.magisk/sepolicy.rules/$MODNAME/sepolicy.rule" ]; then
-    magiskpolicy --live --apply "$MODDIR/sepolicy.rule"
 fi
 
 . "$MODDIR/resetprop.sh"
