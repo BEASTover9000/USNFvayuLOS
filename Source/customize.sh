aa72ae704241cabade5f71365e0aa63fec1a6991
ToucH <85633117+ToucH9000@users.noreply.github.com>
2023-11-13 13:49:26 +0500

    Add files via upload

--- Source/customize.sh
+++ Source/customize.sh
@@ -1,14 +1,21 @@
-#!/system/bin/sh
-
-# Android 8.0 or newer
-if [[ "$(getprop ro.build.version.sdk)" -lt 26 ]]; then
-    ui_print ""
-    ui_print "Functionality is limited on Android 7 and older."
-    ui_print "Hardware-backed attestation will not be disabled."
-    ui_print ""
-
-    # Remove Zygisk module, but keep props and scripts
-    rm -fr "$MODPATH/zygisk"
+if $BOOTMODE; then
+    ui_print "- Installing from Magisk / KernelSU app"
+else
+    ui_print "*********************************************************"
+    ui_print "! Install from recovery is NOT supported"
+    ui_print "! Recovery sucks"
+    ui_print "! Please install from Magisk / KernelSU app"
+    abort    "*********************************************************"
 fi
 
-chmod 755 "$MODPATH/service.sh" "$MODPATH/post-fs-data.sh"
+# Android < 8.0 | No Zygisk
+if [ "$API" -lt 26 ]; then
+    ui_print "*********************************************************"
+    ui_print "! Functionality is limited on Android 7 and older"
+    ui_print "! Hardware-backed attestation will not be disabled"
+    ui_print "! MODEL & FINGERPRINT props will not be modified"
+    ui_print "*********************************************************"
+
+    # Remove Zygisk part but keep props and scripts
+    rm -rf "$MODPATH/zygisk"
+fi
